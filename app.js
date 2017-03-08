6974ea01b7aed229b3d09656b4baff6026df974a
Stivali Serna <stivaliserna08@gmail.com>
2017-03-08 10:11:37 +0100

    Create Mongoose schema and endpoints

---
+++ app.js
@@ -0,0 +1,39 @@
+const Koa = require('koa')
+const Router = require('koa-router')
+const bodyParser = require('koa-bodyparser')
+const mongoose = require('mongoose')
+const cors = require('koa2-cors')
+
+const Product = require('./models/product.model')
+
+const PORT = 3001
+const DB_URI = 'mongodb://localhost:27017/hub'
+
+const app = new Koa()
+const router = new Router()
+
+router
+  .get('/products', async (ctx, next) => {
+    ctx.response.body = await Product.find().exec()
+    ctx.response.status = 200
+    return next()
+  })
+  .post('/products', async (ctx, next) => {
+    const newProduct = new Product(ctx.request.body)
+    ctx.response.body = await newProduct.save()
+    ctx.response.status = 200
+    return next()
+  })
+
+app.use(cors())
+app.use(bodyParser())
+app.use(router.routes())
+
+mongoose.Promise = global.Promise
+mongoose.connection
+  .on('error', console.error)
+  .on('close', () => console.log('Database connection closed.'))
+  .once('open', () => {
+    app.listen(PORT, () => console.log(`Listening at PORT ${PORT}`))
+  })
+mongoose.connect(DB_URI)
---
+++ models/product.model.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+mongoose.Promise = global.Promise
+
+const ProductSchema = new mongoose.Schema({
+  productID: { type: String, required: true },
+  description: { type: String, required: true },
+  minOrderQuantity: { type: Number, required: true },
+  productUnit: { type: String, required: true },
+  productUnitQuantity: { type: Number, required: true },
+  price: { type: Number, required: true },
+  base64Img: String
+}, { collection: 'products' })
+
+module.exports = mongoose.model('Product', ProductSchema)
