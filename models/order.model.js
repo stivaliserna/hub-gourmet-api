05ae7480449e6f551da8f6d1b6b1b0da801af9c7
Stivali Serna <stivaliserna08@gmail.com>
2017-03-14 13:57:48 +0100

    Update order model using Mongoose virtuals

--- models/order.model.js
+++ models/order.model.js
@@ -6,12 +6,19 @@ const OrderSchema = new mongoose.Schema({
   name: { type: String, required: true },
   surname: { type: String, required: true },
   identificationType: { type: String, required: true },
-  identificationNumber: { type: Number, required: true },
+  identificationNumber: { type: String, required: true },
   mail: { type: String, required: true },
-  telephone: { type: Number, required: true },
-  products: { type: String, required: true },
-  quantity: { type: Number, required: true },
-  total: { type: Number, required: true }
+  telephone: { type: String, required: true },
+  items: [{
+    price: { type: Number, required: 'true' },
+    quantity: { type: Number, required: 'true' }
+  }]
 }, {collection: 'orders'})
+
+OrderSchema.virtual('items.product', {
+  ref: 'Product',
+  localField: 'product',
+  foreignField: 'productID'
+})
 
 module.exports = mongoose.model('Order', OrderSchema)
